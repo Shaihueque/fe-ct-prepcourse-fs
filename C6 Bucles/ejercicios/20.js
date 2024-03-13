820272020fe39d1171c50ff2084aef5164198c78
Shaiuheque <shaihueque@gmail.com>
2024-03-13 11:25:47 -0300

    haciendo ejercicios

--- C6 Bucles/ejercicios/20.js
+++ C6 Bucles/ejercicios/20.js
@@ -3,6 +3,16 @@ function sumarHastaNConBreak(n) {
    // Devuelve la suma de todos los números desde 1 hasta n.
    // Si la suma supera a 100, detén el bucle usando break.
    // Tu código:
+
+   let sum = 0;
+
+   for(let i=1; i<=n; i++){
+      sum += i;
+      if(sum >=100){
+         break;
+      }
+   }
+   return sum;
 }
 
 module.exports = sumarHastaNConBreak;
