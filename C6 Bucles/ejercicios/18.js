4c7f0d2efc9e6c4efb00da74890e75549718411d
Shaiuheque <shaihueque@gmail.com>
2024-03-18 21:01:19 -0300

    hasta ejercicio 21

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,18 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+
+  if (a > b) {
+    let revez = a;
+    a = b;
+    b = revez;
+  }
+  let resultado = 1;
+
+  for (let i = a; i <= b; i++) {
+    resultado *= i;
+  }
+  return Math.abs(resultado);
 }
 
 module.exports = productoEntreNúmeros;
