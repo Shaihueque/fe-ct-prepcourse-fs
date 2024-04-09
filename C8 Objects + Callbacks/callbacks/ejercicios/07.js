ddf1d775759cf0733c77b7783d5ad22fe315e7b1
Shaiuheque <shaihueque@gmail.com>
2024-04-09 14:50:11 -0300

    Ejercicios terminados, unidad 8

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,14 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   var nuevoArreglo = [];
+
+   for( let i = 0; i < arrayOfStrings.length; i++){
+      if( arrayOfStrings[i][0] === "a")
+      nuevoArreglo.push(arrayOfStrings[i]);
+   }
+   return nuevoArreglo;
 }
 
 module.exports = filter;
